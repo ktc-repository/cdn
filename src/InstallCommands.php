be41a4315e2af1e980e155680d072363d5b86bed
Christian Irack <christian.irack@ktc.com.mx>
2017-12-08 12:51:59 -0600

    Update v1.0.20

--- src/InstallCommands.php
+++ src/InstallCommands.php
@@ -31,36 +31,44 @@ class InstallCommands extends Command
      */
     public function handle()
     {
-        //$this->exportViews();
-        copy(__dir__."/templates/Envoy.blade.php", base_path()."/Envoy.blade.php");
-        chmod(base_path()."/Envoy.blade.php", 0777);
-      
-        chmod(base_path()."/.env", 0777);
 
-        $directory = $this->ask('Name of the new CDN, no special characters or spaces (example: new_client');
-        $sshUser = $this->ask('Write your SSH user (example: root@new-dev.ktcagency.com');
-        //$pathSSH = $this->ask('Type the path to your puprivada ssh key (example: /Users/John/.ssh/id_rsa)');
-        $pathSSH = exec('echo $HOME/.ssh/id_rsa');
-
-         
-        $getUser = str_replace('ssh ','',$sshUser);
-        $getUser = explode("@",$sshUser);
-        $user = $getUser[0];
-        $host = $getUser[1];
-
+        
         $templateFile = file_get_contents(__dir__."/templates/envSample.txt");
-
+        
         $check = strstr($templateFile, 'SSH_PRIVATE_KEY_PATH');
+
         if($check==false OR $check==''){
+
+            copy(__dir__."/templates/Envoy.blade.php", base_path()."/Envoy.blade.php");
+            chmod(base_path()."/Envoy.blade.php", 0777);
+        
+            chmod(base_path()."/.env", 0777);
+
+            $directory = $this->ask('Name of the new CDN, no special characters or spaces (example: new_client');
+            $sshUser = $this->ask('Write your SSH user (example: root@new-dev.ktcagency.com');
+            //$pathSSH = $this->ask('Type the path to your puprivada ssh key (example: /Users/John/.ssh/id_rsa)');
+            $pathSSH = exec('echo $HOME/.ssh/id_rsa');
+
+            
+            $getUser = str_replace('ssh ','',$sshUser);
+            $getUser = explode("@",$sshUser);
+            $user = $getUser[0];
+            $host = $getUser[1];
+
+            
             $templateFile = str_replace('CDN_BASE_SUBDIRECTORY=', 'CDN_BASE_SUBDIRECTORY="'.ltrim($directory).'"', $templateFile);
             $templateFile = str_replace('CDN_SSH_USERNAME=', 'CDN_SSH_USERNAME="'.ltrim($user).'"', $templateFile);
             $templateFile = str_replace('CDN_SSH_HOST=', 'CDN_SSH_HOST="'.ltrim($host).'"', $templateFile);
             $templateFile = str_replace('CDN_SSH_PRIVATE_KEY_PATH=', 'CDN_SSH_PRIVATE_KEY_PATH="'.ltrim($pathSSH).'"', $templateFile);
             file_put_contents(base_path()."/.env", $templateFile, FILE_APPEND);
+
+            
             $this->info('Creating the remote directory...');
             $runTask = exec('envoy run cdn');
             $this->info('Public URL: '.env('CDN_PUBLIC').'/'.env('CDN_BASE_SUBDIRECTORY'));
             $this->info($runTask);
+
+
         }else{
 
             $this->info('It could not start a new installation. Existing CDN variables in your .env');
