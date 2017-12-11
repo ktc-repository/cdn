33c070a09703be845f5a8a05069a7b1d2cc4938e
Christian Irack <christian.irack@ktc.com.mx>
2017-12-11 11:49:49 -0600

    Update v1.0.24

--- src/CustomHelpers.php
+++ src/CustomHelpers.php
@@ -3,4 +3,10 @@ function CDN($file)
 {
     return env('CDN_PUBLIC').'/'.env('CDN_BASE_SUBDIRECTORY').'/'.$file;
 }
+
+function CDN_EXT($file)
+{
+    $getExt = explode('.',$file);
+    return strtolower(end($getExt));
+}
 ?>
