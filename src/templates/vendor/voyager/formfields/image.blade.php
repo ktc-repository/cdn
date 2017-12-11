31cd773234a86c85dae1bf33acafbeed3158e818
Christian Irack <christian.irack@ktc.com.mx>
2017-12-11 15:00:02 -0600

    Update v1.0.34

--- src/templates/vendor/voyager/formfields/image.blade.php
+++ src/templates/vendor/voyager/formfields/image.blade.php
@@ -1,6 +1,7 @@
 @if(isset($dataTypeContent->{$row->field}))
-    <img src="@if( !filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $dataTypeContent->{$row->field} ) }}@else{{ $dataTypeContent->{$row->field} }}@endif"
-         style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;">
+<br>
+    <img src="@if( !filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL)){{ CDN( $dataTypeContent->{$row->field} ) }}@else{{ CDN($dataTypeContent->{$row->field}) }}@endif"
+        width="300">
 @endif
-
+<br>
 <input @if($row->required == 1 && !isset($dataTypeContent->{$row->field})) required @endif type="file" data-name="{{ $row->display_name }}"  name="{{ $row->field }}">
--- src/templates/vendor/voyager/formfields/multiple_images.blade.php
+++ src/templates/vendor/voyager/formfields/multiple_images.blade.php
@@ -4,9 +4,10 @@
     @if($images != null)
         @foreach($images as $image)
             <div class="img_settings_container" data-field-name="{{ $row->field }}">
-                <img src="{{ Voyager::image( $image ) }}" data-image="{{ $image }}" data-id="{{ $dataTypeContent->id }}">
+                <img src="{{ CDN( $image ) }}" data-image="{{ $image }}" data-id="{{ $dataTypeContent->id }}" width="300">
                 <a href="#" class="voyager-x remove-multi-image"></a>
             </div>
+            <br>
         @endforeach
     @endif
 @endif
